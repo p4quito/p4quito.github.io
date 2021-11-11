801c2273516576943bd3fe2947603ebb68888868
jeft098 <jefferson.rengifo@estudiante.uam.es>
2021-11-11 19:37:54 +0100

    cambios finales

--- codigo.js
+++ codigo.js
@@ -3,13 +3,17 @@ function addItem() {
     const tipo = document.getElementById("options").value
     console.log(cantidad)
     console.log(tipo)
+
     const fila = document.createElement("tr")
     const c1 = document.createElement("td")
-    c1.innerText = cantidad
+    c1.innerText = tipo
     const c2 = document.createElement("td")
-    c2.innerText = tipo
+    c2.innerText = cantidad
     fila.appendChild(c1)
     fila.appendChild(c2)
+
+    var contenido = document.getElementById("items")
+    contenido.appendChild(fila)
 
 
 }
